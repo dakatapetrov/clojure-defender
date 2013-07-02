d4bfa7d1401c318c1e7836983a70d7bddd545b9b
dakatapetrov <daka7a@gmail.com>
2013-07-03 02:52:46 +0300

    Initialize global variables.

---
+++ src/clojure_defender/globals.clj
@@ -0,0 +1,20 @@
+(ns clojure-defender.globals)
+
+(use 'clojure.repl)
+(use 'seesaw.core)
+
+(def paths (atom []))
+(def protect-points (atom []))
+(def build-areas (atom []))
+(def generators (atom []))
+(def buildings (atom []))
+(def enemies (atom []))
+(def projectiles (atom []))
+
+(def window-width 800.0)
+(def window-height 600.0)
+(def building-size 10.0)
+(def enemies-size 5.0)
+(def enemies-max-speed 0.0)
+(def projectiles-max-speed 0.0)
+(def main-frame (frame :title "Clojure Defender" :size [window-width :by window-height]))
