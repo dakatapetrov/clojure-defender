3fa572a4ef22cc0f8d248d63b99506d3b568668b
dakatapetrov <daka7a@gmail.com>
2013-07-03 02:57:10 +0300

    Level 01 and GUI are now loaded in the core.

--- src/clojure_defender/core.clj
+++ src/clojure_defender/core.clj
@@ -1,7 +1,14 @@
 (ns clojure-defender.core
   (:gen-class))
 
+(use 'clojure.repl)
+(use '[seesaw core color graphics behave keymap])
+(require '[clojure-defender.globals :as gl])
+(use 'clojure-defender.gui.main)
+(use 'clojure-defender.levels.level-01)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (load-level)
+  (create-gui))
