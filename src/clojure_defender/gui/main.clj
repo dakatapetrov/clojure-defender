81e9bedbb30a5faa1e238618ff3ccd16bef9a74f
dakatapetrov <daka7a@gmail.com>
2013-07-04 00:45:48 +0300

    Now draws enemies.

--- src/clojure_defender/gui/main.clj
+++ src/clojure_defender/gui/main.clj
@@ -9,47 +9,47 @@
   (config! fr :content content)
   content)
 
-(defn draw-rectangle
-  [g x y dx dy s]
-  (draw g (polygon [x y] [(+ x dx) y] [(+ x dx) (+ y dy)] [x (+ y dy)]) s))
-
-(defn draw-square
-  [g x y d s]
-  (draw-rectangle g x y d d s))
-
 (defn draw-play-area
-  [c g]
-  (draw-rectangle g 0 0 800 600 (style :background "#3eb677")))
+  [g]
+  (draw g (rect 0 0 gl/window-width gl/window-height) (style :background "#3eb677")))
 
 
 (defn draw-paths
-  [c g]
+  [g]
   (doseq [path @gl/paths]
     (let [x (:x path)
           y (:y path)]
-      (draw-square g x y 30 (style :background "#e39e54")))))
+      (draw g (rect x y gl/path-size) (style :background "#e39e54")))))
 
 (defn draw-protect-points
-  [c g]
+  [g]
   (doseq [protect-point @gl/protect-points]
     (let [x (:x protect-point)
           y (:y protect-point)]
-      (draw-square g x y 30 (style :background :red)))))
+      (draw g (rect x y gl/path-size) (style :background :red)))))
 
 (defn draw-build-areas
-  [c g]
+  [g]
   (doseq [build-area @gl/build-areas]
     (let [x (:x build-area)
           y (:y build-area)]
-      (draw-square g x y 30 (style :background "#f4dd65")))))
+      (draw g (rect x y gl/building-size) (style :background "#f4dd65")))))
+
+(defn draw-enemies
+  [g]
+  (doseq [enemy (keys @gl/enemies)]
+    (let [x (:x @enemy)
+          y (:y @enemy)]
+      (draw g (rect x y gl/enemy-size) (style :background :blue)))))
 
 
 (defn draw-world
   [c g]
-  (draw-play-area c g)
-  (draw-paths c g)
-  (draw-protect-points c g)
-  (draw-build-areas c g))
+  (draw-play-area g)
+  (draw-paths g)
+  (draw-protect-points g)
+  (draw-build-areas g)
+  (draw-enemies g))
   ; (draw g (polygon [0 0] [30 0] [30 30] [0 30]) (style :background "#464248"))
   ; (draw g (polygon [10 10] [20 10] [20 20] [10 20]) (style :background :red)))
 
@@ -58,6 +58,10 @@
     :center (canvas :paint draw-world
               :class :world
               :background :black)))
+
+(defn redisplay [root]
+  (dosync
+    (config! (select root [:.world]) :paint draw-world)))
 
 (defn create-gui
   []
