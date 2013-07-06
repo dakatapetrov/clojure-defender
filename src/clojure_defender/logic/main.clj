982103abdf0b5347d2cae7d6a3c4d214c2fcf3df
dakatapetrov <daka7a@gmail.com>
2013-07-07 02:13:41 +0300

    Majore improvements. Add building and projectile logic.

--- src/clojure_defender/logic/main.clj
+++ src/clojure_defender/logic/main.clj
@@ -2,6 +2,12 @@
 
 (use 'clojure-defender.gui.main)
 (require '[clojure-defender.globals :as gl])
+(require '[clojure-defender.data.buildings :as db])
+
+(defn location
+  [object]
+  (let [{:keys [x y]} object]
+    [x y]))
 
 (defn in-rect?
   [x y w h ox oy]
@@ -12,76 +18,156 @@
          (not-pos? (compare y oy))
          (not-neg? (compare (+ y h) oy)))))
 
-(defn get-path
-  [enemy]
+(defn on-object
+  [x y objects]
   (first (filter #(in-rect? (:x %)
                             (:y %)
                             gl/path-size
                             gl/path-size
-                            (:x @enemy)
-                            (:y @enemy))
-                 @gl/paths)))
-
-(defn on-defend-point?
-  [enemy]
-  (some #(in-rect? (:x %)
-                   (:y %)
-                   gl/path-size
-                   gl/path-size
-                   (:x @enemy)
-                   (:y @enemy))
-        @gl/defend-points))
+                            x
+                            y)
+                 objects)))
 
 (defn move-enemy
   [enemy]
-  (let [speed (:speed @enemy)
-        path (get-path enemy)
-        px (first (:direction path))
-        py (second (:direction path))
+  (let [{:keys [speed hp x y]} @enemy
+        on-path (on-object x y @gl/paths)
+        [px py] (:direction on-path)
+        on-defend-point (on-object x y @gl/defend-points)
         movex (* speed px)
         movey (* speed py)]
-    (if (on-defend-point? enemy)
-      (swap! gl/enemies dissoc enemy)
+    (if (or (<= hp 0) on-defend-point)
+      (swap! gl/enemies disj enemy)
       (dosync
-        (do
-          (alter enemy update-in [:x] #(+ % movex))
-          (alter enemy update-in [:y] #(+ % movey)))))))
+        (alter enemy update-in [:x] #(+ % movex))
+        (alter enemy update-in [:y] #(+ % movey))))))
 
 (defn step
   []
-  (doseq [enemy (keys @gl/enemies)]
+  (doseq [enemy @gl/enemies]
     (move-enemy enemy)))
+
+(defn cooldown-timer
+  [col]
+  (doseq [obj col]
+    (let [ac (:ac obj)]
+      (if @gl/playing?
+        (when (pos? @ac)
+          (swap! ac dec))))))
 
 
 (defn spawn
   []
   (doseq [spawner @gl/spawners]
-    (let [ac (:active-cooldown spawner)]
-      (when (realized? @ac)
-        (let [x (:x spawner)
-              y (:y spawner)
-              enemy (rand-nth (:enemies spawner))
-              dir (rand-nth (:directions spawner))
-              cooldown (:cooldown spawner)]
+    (let [ac (:ac spawner)]
+      (when-not (pos? @ac)
+        (let [{:keys [x y cooldown]} spawner
+              enemy (rand-nth (:enemies spawner))]
           (swap! gl/enemies
                  conj
-                 {(ref (conj enemy {:x x :y y})) :on})
-          (reset! ac (future (Thread/sleep cooldown))))))))
+                 (ref (conj enemy {:x x :y y})))
+          (reset! ac cooldown))))))
 
-(defn respawn
+(defn build
   []
-  (let [cooldown (atom (future (true)))]
-    (if (realized? @cooldown)
-      (do
-        (spawn)
-        (reset! cooldown (future (Thread/sleep 1000)))))))
+  (let [[cx cy] (location @gl/click-location)
+        on-build-area (on-object cx cy @gl/build-areas)
+        [x y] (location on-build-area)
+        on-building (on-object cx cy @gl/buildings)]
+    (when (and x y)
+      (if-not on-building
+        (dosync
+          (swap! gl/buildings conj (conj @gl/current-building
+                                         {:x x :y y :ac (atom 0)}))
+          (alter gl/click-location conj {:x nil :y nil}))
+        (dosync
+          (swap! gl/buildings disj on-building)
+          (alter gl/click-location conj {:x nil :y nil}))))))
+
+(defn abs
+  [x]
+  (if (neg? x) (- x) x))
+
+(defn get-direcction
+  [x y ox oy]
+  (let [vx (- x ox)
+        vy (- y oy)]
+    (if (< (abs vx) (abs vy))
+      [(/ vx (abs vy)) (/ vy (abs vy))]
+      [(/ vx (abs vx)) (/ vy (abs vx))])))
+
+(defn enemies-in-range
+  [x y r]
+  (filter #(let [[ex ey] (location @%)]
+             (<= (+ (* (- ex x)
+                       (- ex x))
+                    (* (- ey y)
+                       (- ey y)))
+                 (* r r)))
+          @gl/enemies))
+
+(defn rect-center
+  [x y w h]
+  (let [cx (+ x w)
+        cy (+ y h)]
+    [cx cy]))
+
+
+(defn shoot
+  []
+  (doseq [building @gl/buildings]
+    (let [[x y] (location building)
+          [cx cy] (rect-center x y 15 15)
+          cd (:cooldown building)
+          ac (:ac building)
+          projectile (-> building :projectiles rand-nth)
+          fire-range (:fire-range building)
+          enemies (enemies-in-range x y fire-range)
+          one (first enemies)]
+      (when (and one (not (pos? @ac)))
+        (swap! gl/projectiles conj (ref (conj projectile
+                                              {:x cx :y cy :enemy one})))
+        (reset! ac cd)))))
+
+(defn move-projectiles
+  []
+  (doseq [projectile @gl/projectiles]
+    (dosync
+      (let [{:keys [x y speed enemy]} @projectile
+            [ex ey] (location @enemy)
+            [cx cy] (rect-center ex ey 5 5)
+            [dirx diry] (get-direcction cx cy x y)]
+        (alter projectile update-in [:x] #(+ % (* speed dirx)))
+        (alter projectile update-in [:y] #(+ % (* speed diry)))))))
+
+(defn collisions
+  []
+  (doseq [p @gl/projectiles]
+    (let [{:keys [x y damage enemy]} @p
+          [ex ey] (location @enemy)
+          [cx cy] (rect-center ex ey 5 5)
+          dx (* (- x cx) (- x cx))
+          dy (* (- y cy) (- y cy))
+          d (+ dx dy)]
+      (when (<= d 10)
+        (dosync
+          (alter enemy update-in [:hp] #(- % damage))
+          (swap! gl/projectiles disj p))))))
+
 
 (defn play
   []
   (loop [painter (atom (future (true)))]
-    (spawn)
-    (step)
-    (when (realized? @painter)
-      (reset! painter (future (redisplay gl/main-frame))))
+    (when @gl/playing?
+      (cooldown-timer @gl/spawners)
+      (cooldown-timer @gl/buildings)
+      (spawn)
+      (build)
+      (step)
+      (shoot)
+      (move-projectiles)
+      (collisions)
+      (when (realized? @painter)
+        (reset! painter (future (redisplay gl/main-frame)))))
     (Thread/sleep 3)
     (recur painter)))
