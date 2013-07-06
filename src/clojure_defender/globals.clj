79a4250740bf9fd26a2b15a7fb887b6be448ad96
dakatapetrov <daka7a@gmail.com>
2013-07-07 02:11:30 +0300

    More features = more global variables (:

--- src/clojure_defender/globals.clj
+++ src/clojure_defender/globals.clj
@@ -2,21 +2,25 @@
 
 (use 'clojure.repl)
 (use 'seesaw.core)
+(require '[clojure-defender.data.buildings :as db])
 
 (def paths (atom []))
 (def defend-points (atom []))
 (def build-areas (atom []))
 (def spawners (atom []))
-(def buildings (atom []))
-(def enemies (atom {}))
-(def projectiles (atom []))
+(def buildings (atom #{}))
+(def enemies (atom #{}))
+(def projectiles (atom #{}))
 
-(def window-width 800.0)
-(def window-height 600.0)
+(def window-width 1024)
+(def window-height 768)
 (def path-size 30.0)
 (def building-size 30.0)
 (def enemy-size 10.0)
-(def enemy-max-speed 1.0)
-(def projectiles-max-speed 0.0)
+(def projectile-size 3.0)
 (def main-frame (frame :title "Clojure Defender"
+                       :on-close :exit
                        :size [window-width :by window-height]))
+(def click-location (ref {}))
+(def current-building (atom db/fire-tower))
+(def playing? (atom false))
