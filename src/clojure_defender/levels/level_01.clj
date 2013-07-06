502aab1b217cbb2d0b453202efaf68788e619152
dakatapetrov <daka7a@gmail.com>
2013-07-07 02:12:16 +0300

    Make use of enemy, building and projectile data.

--- src/clojure_defender/levels/level_01.clj
+++ src/clojure_defender/levels/level_01.clj
@@ -1,28 +1,31 @@
 (ns clojure-defender.levels.level-01)
 
 (require '[clojure-defender.globals :as gl])
+(require '[clojure-defender.data.enemies :as ed])
+
+(def world {:x 0 :y 0 :width 980 :height 768 :color "#3eb677"})
 
 (def paths
   (vec (concat (for [x (range 0 700 30)
                       y [100 490]]
-                  {:x x :y y :direction [1 0]})
-                [{:x 720 :y 100 :direction [0.5 1]}]
+                  {:x x :y y :direction [1 0] :color "#e39e54"})
+                [{:x 720 :y 100 :direction [0.5 1] :color "#e39e54"}]
                 (for [x [720]
                       y (range 130 500 30)]
-                  {:x x :y y :direction [0 1]}))))
+                  {:x x :y y :direction [0 1] :color "#e39e54"}))))
 
-(def defend-points [{:x 720 :y 490 :capacity 50}])
+(def defend-points [{:x 720 :y 490 :capacity 50 :color :red}])
 
 (def build-areas
   (for [x (range 0 700 90)
         y [70 130]]
-    {:x x :y y}))
+    {:x x :y y :color "#f4dd65"}))
 
 (def spawners
-  [{:x 0 :y 110 :enemies [{:speed 0.1} {:speed 0.3}]
-    :cooldown 1000 :active-cooldown (atom (future (true)))}
-   {:x 0 :y 500 :enemies [{:speed 0.1} {:speed 0.3}]
-    :cooldown 3000 :active-cooldown (atom (future (true)))}])
+  [{:x 0 :y 110 :enemies [ed/assassin ed/zombie ed/orc]
+    :cooldown 600 :ac (atom 0)}
+   {:x 0 :y 500 :enemies [ed/assassin ed/zombie]
+    :cooldown 2000 :ac (atom 0)}])
 
 (def buildings)
 (def enemies)
