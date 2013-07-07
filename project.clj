fea010a6032a05e39426cad6eba5be96d76609d2
dakatapetrov <daka7a@gmail.com>
2013-07-07 18:06:39 +0300

    Add clojure-contrib as a dependency.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [seesaw "1.4.3"]]
   :main clojure-defender.core)
