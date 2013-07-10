c35211f7539cf03745af00baa78fd69bff92ca6e
dakatapetrov <daka7a@gmail.com>
2013-07-11 02:10:58 +0300

    Refactor enemy logic.

--- src/clojure_defender/logic/enemy.clj
+++ src/clojure_defender/logic/enemy.clj
@@ -5,14 +5,32 @@
 (require '[clojure-defender.globals :as gl])
 (use 'clojure-defender.physics.geometry)
 
+(defn- path-direction
+  [x y]
+  (let [on-path (on-object x y @gl/paths)
+        [dir-x dir-y] (:direction on-path)]
+    [dir-x dir-y]))
+
+(defn- on-defend-point?
+  [x y]
+  (on-object x y @gl/defend-points))
+
+(defn- move-by
+  [dir-x dir-y speed]
+  (let [move-by-x (* speed dir-x)
+        move-by-y (* speed dir-y)]
+    [move-by-x move-by-y]))
+
+(defn- dead?
+  [enemy]
+  (let [hp (:hp @enemy)]
+    (<= hp 0)))
+
 (defn step-enemy
   [enemy]
-  (let [{:keys [speed hp x y]} @enemy
-        on-path (on-object x y @gl/paths)
-        [px py] (:direction on-path)
-        on-defend-point (on-object x y @gl/defend-points)
-        movex (* speed px)
-        movey (* speed py)]
-    (if (or (<= hp 0) on-defend-point)
+  (let [{:keys [speed x y]} @enemy
+        [dir-x dir-y] (path-direction x y)
+        [move-by-x move-by-y] (move-by dir-x dir-y speed)]
+    (if (or (dead? enemy) (on-defend-point? x y))
       (kill-enemy enemy)
-      (move-enemy enemy movex movey))))
+      (move-enemy enemy move-by-x move-by-y))))
