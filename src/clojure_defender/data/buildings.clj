e19559cd975a855f7c6d536fb09aab5ae2adfd4d
dakatapetrov <daka7a@gmail.com>
2013-07-12 00:47:13 +0300

    refactoring.

--- src/clojure_defender/data/buildings.clj
+++ src/clojure_defender/data/buildings.clj
@@ -12,9 +12,10 @@
   (conj building attributes))
 
 (def fire-tower (define-building 100 70 300 [pj/fireball] :orange))
-(def frost-tower (define-building 110 70 350 [pj/frostball pj/frostball pj/frostball pj/slow-frostball] :blue))
+(def frost-tower (define-building 110 70 350
+                                  [pj/frostball pj/frostball pj/frostball pj/slow-frostball]
+                                  :blue))
 (def arcane-tower (define-building 90 65 250 [pj/arcanemissle] :gray))
-(def ultimate-tower (define-building 100 70
-                                   280
-                                   [pj/fireball pj/frostball pj/arcanemissle]
-                                   :black))
+(def ultimate-tower (define-building 100 70 280
+                                     [pj/fireball pj/frostball pj/arcanemissle]
+                                     :black))
